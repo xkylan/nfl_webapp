b6829c2b15c78bdcb279bf8b6e3e8e7ef2e9b113
Kylan Sakata <kylan@DN51u4tu.SUNet>
2021-11-16 20:24:37 -0800

    debugging r backend

--- data_pipeline/seeding_api.R
+++ data_pipeline/seeding_api.R
@@ -4,15 +4,15 @@ library(Rcpp)
 # RSQLite also required
 library(dplyr, warn.conflicts = FALSE)
 library(hash)
-options(digits = 3)
-con <- dbConnect(RSQLite::SQLite(), "leverage.db")
+options(digits = 5)
+con <- dbConnect(RSQLite::SQLite(), "/Users/kylan/Downloads/nfl_website/data_pipeline/leverage.db")
 
 res <- dbSendQuery(con, "SELECT * FROM fpi ORDER BY created_at DESC LIMIT 32;")
 fpi <- as.data.frame(dbFetch(res))
 fpi
 # validate that all 32 teams are in fpi? and/or that there is just one created_at value
-dbClearResult(res)
-dbDisconnect(con)
+#dbClearResult(res)
+#dbDisconnect(con)
 
 fpiMap <- hash::hash()
 
@@ -22,103 +22,68 @@ fpiMapFn <- function(x) {
 
 apply(fpi, 1, fpiMapFn)
 
-fpiMap[['KC']]
-
-calcResult <- function(homeFPI, awayFPI, n){
-  homeSpread <- awayFPI - homeFPI
-  # https://wizardofodds.com/games/sports-betting/nfl/
-  pHomeWin <- exp(-0.14324*homeSpread)/(1+exp(-0.14324*homeSpread))
-  cat(pHomeWin, "\n")
-  return(sample(c(-3,3), n, prob=c(1-pHomeWin,pHomeWin), replace=TRUE))
-}
-
-calcResult(fpi[fpi['team']=='TB','FPI'], fpi[fpi['team']=='JAX','FPI'], 100)
-
-mean(calcResult(fpiMap[["HOU"]],fpiMap[["KC"]],100))
-
 fpi_sim_model <- function(teams, games, week_num, ...) {
   args <- list(...)
-  # fixedHomeTeam <- ""
-  # fixedAwayTeam <- ""
-  # fixedWeek <- -1
-  # fixedResult <- 0
-  # fixGame <- FALSE
-  # if ("fixedHomeTeam" %in% names(args)) {
-  #   fixedHomeTeam <- args$fixedHomeTeam
-  # }
-  # 
-  # if ("fixedAwayTeam" %in% names(args)) {
-  #   fixedAwayTeam <- args$fixedAwayTeam
-  # }
-  # 
-  # if ("fixedWeek" %in% names(args)) {
-  #   fixedWeek <- args$fixedWeek
-  # }
-  # 
-  # if ("fixedResult" %in% names(args)) {
-  #   fixedResult <- args$fixedResult
-  # }
-  # 
-  # if (fixedHomeTeam != "" & fixedAwayTeam != "" & fixedWeek != -1 & fixedResult != 0) {
-  #   fixGame <- TRUE
-  # }
+
+  calcResult <- function(homeFPI, awayFPI, n){
+    homeSpread <- awayFPI - homeFPI
+    # https://wizardofodds.com/games/sports-betting/nfl/
+    pHomeWin <- exp(-0.14324*homeSpread)/(1+exp(-0.14324*homeSpread))
+    #cat(pHomeWin, "\n")
+    return(sample(c(-3,3), n, prob=c(1-pHomeWin,pHomeWin), replace=TRUE))
+  }
   
   games <- games %>%
     dplyr::mutate(
       result = dplyr::case_when(
         !is.na(result) | week != week_num ~ result,
-        # fixGame & week == fixedWeek & away_team == fixedAwayTeam & home_team == fixedHomeTeam ~ fixedResult,
-        TRUE ~ calcResult(fpi[fpi['team']==home_team,'FPI'], fpi[fpi['team']==away_team,'FPI'], n())
+        TRUE ~ calcResult((values(fpiMap, keys=c(home_team))), (values(fpiMap, keys=c(away_team))), n())
+        #TRUE ~ sample(c(-3, 3), n(), prob = c(0.9, 0.1), replace = TRUE)
       )
     )
   
   return(list(teams=teams, games=games))
 }
 
-kc <- fpi[fpi['team']=='KC','FPI']
-tb <- fpi[fpi['team']=='HOU','FPI']
-spread <- kc - tb
-wp <- exp(-0.14324*spread)/(1+exp(-0.14324*spread))
-mean(sample(c(-3,3), 1000, prob=c(1-wp,wp), replace=TRUE))
-
-fpi
 sims <- simulate_nfl(
   nfl_season=2020,
   process_games = fpi_sim_model,
   fresh_season = TRUE,
-  simulations = 1000
+  simulations = 10,
+  test_week = 5,
+  print_summary = TRUE
 )
 
-sims$games %>%
-  filter(game_type == 'REG') %>%
-  group_by(week, home_team, away_team) %>%
-  summarise(result = mean(result)) %>%
-  arrange(result)
+# sims$games %>%
+#   filter(game_type == 'REG') %>%
+#   group_by(week, home_team, away_team) %>%
+#   summarise(result = mean(result)) %>%
+#   arrange(result)
 
 sims$overall %>% arrange(wins)
 
-pi
-
-x <- 0
-
-for(i in 1:100){
-  x <- x + sum(calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='CLE','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='BAL','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='LAC','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='PHI','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='BUF','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='WAS','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='TEN','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='NYG','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='GB','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='LV','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='DAL','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='DEN','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='LV','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='LAC','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='PIT','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='CIN','FPI'], 1),
-                calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='DEN','FPI'], 1))
-}
-
-x/100
+# pi
+# 
+# x <- 0
+# 
+# for(i in 1:100){
+#   x <- x + sum(calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='CLE','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='BAL','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='LAC','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='PHI','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='BUF','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='WAS','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='TEN','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='NYG','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='GB','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='LV','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='DAL','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='DEN','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='LV','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='LAC','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='PIT','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='CIN','FPI'], 1),
+#                 calcResult(fpi[fpi['team']=='KC','FPI'], fpi[fpi['team']=='DEN','FPI'], 1))
+# }
+# 
+# x/100
