58bf377e7678c7bd050a3e3aee0b9230f96af66d
Kylan Sakata <kylan808@gmail.com>
2023-03-31 16:10:25 -0700

    testing ga4-react package

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GA4React, { useGA4React } from "ga-4-react";
+
+const ga4react = new GA4React('G-GK6RKKDPE7');
+
+const ga = useGA4React();
+console.log(ga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+(async () => {
+  await ga4react.initialize();
+
+  root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-);
+  );
+})();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
